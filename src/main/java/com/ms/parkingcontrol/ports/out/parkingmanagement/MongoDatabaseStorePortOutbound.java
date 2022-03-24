38faefb5f18aceebd5d3c54c8c4c99c396c599e8
Caio Antonio <caio.antonio.c@outlook.com>
2022-03-24 00:09:41 -0300

    refactor: applied design patterns

---
+++ src/main/java/com/ms/parkingcontrol/ports/out/parkingmanagement/MongoDatabaseStorePortOutbound.java
@@ -0,0 +1,13 @@
+package com.ms.parkingcontrol.ports.out.parkingmanagement;
+
+import com.ms.parkingcontrol.adapters.out.parkingmanagement.ParkingSpot;
+
+import java.util.Optional;
+
+public interface MongoDatabaseStorePortOutbound {
+    Optional<ParkingSpot> searchByLicensePlateCar(String licensePlateCar);
+    Optional<ParkingSpot> searchByParkingSpotNumber(String parkingSpotNumber);
+    Optional<ParkingSpot> searchByApartment(String apartment);
+    Optional<ParkingSpot> searchByBlock(String block);
+    ParkingSpot saveParkingSpot(com.ms.parkingcontrol.domain.parkingmanagement.ParkingSpot parkingSpot);
+}
