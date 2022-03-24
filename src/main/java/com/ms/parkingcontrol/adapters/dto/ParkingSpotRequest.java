5ed4f37c34c2ba181b04792f9c204ebfb2008953
Caio Antonio <caio.antonio.c@outlook.com>
2022-03-24 00:08:23 -0300

    refactor: applied design patterns

--- src/main/java/com/ms/parkingcontrol/adapters/dto/ParkingSpotRequest.java
+++ src/main/java/com/ms/parkingcontrol/adapters/dto/ParkingSpotRequest.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
-public class ParkingSpotDto {
+public class ParkingSpotRequest {
     @NotBlank
     @JsonProperty("parking_spot_number")
     private String parkingSpotNumber;
@@ -41,71 +41,31 @@ public class ParkingSpotDto {
         return parkingSpotNumber;
     }
 
-    public ParkingSpotDto setParkingSpotNumber(String parkingSpotNumber) {
-        this.parkingSpotNumber = parkingSpotNumber;
-        return this;
-    }
-
     public String getLicensePlateCar() {
         return licensePlateCar;
-    }
-
-    public ParkingSpotDto setLicensePlateCar(String licensePlateCar) {
-        this.licensePlateCar = licensePlateCar;
-        return this;
     }
 
     public String getBrandCar() {
         return brandCar;
     }
 
-    public ParkingSpotDto setBrandCar(String brandCar) {
-        this.brandCar = brandCar;
-        return this;
-    }
-
     public String getModelCar() {
         return modelCar;
-    }
-
-    public ParkingSpotDto setModelCar(String modelCar) {
-        this.modelCar = modelCar;
-        return this;
     }
 
     public String getColorCar() {
         return colorCar;
     }
 
-    public ParkingSpotDto setColorCar(String colorCar) {
-        this.colorCar = colorCar;
-        return this;
-    }
-
     public String getResponsibleName() {
         return responsibleName;
-    }
-
-    public ParkingSpotDto setResponsibleName(String responsibleName) {
-        this.responsibleName = responsibleName;
-        return this;
     }
 
     public String getApartment() {
         return apartment;
     }
 
-    public ParkingSpotDto setApartment(String apartment) {
-        this.apartment = apartment;
-        return this;
-    }
-
     public String getBlock() {
         return block;
-    }
-
-    public ParkingSpotDto setBlock(String block) {
-        this.block = block;
-        return this;
     }
 }
