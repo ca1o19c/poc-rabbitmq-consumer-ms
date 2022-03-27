1883dcb2a1f65fd26feb4b56ea3c9c52ab8dc5b3
Caio Antonio <caio.antonio.c@outlook.com>
2022-03-26 21:16:50 -0300

    feat: retrieve parking spots filtered

--- src/main/java/com/ms/parkingcontrol/adapters/in/parkingmanagement/ParkingSpotController.java
+++ src/main/java/com/ms/parkingcontrol/adapters/in/parkingmanagement/ParkingSpotController.java
@@ -1,16 +1,23 @@
 package com.ms.parkingcontrol.adapters.in.parkingmanagement;
 
+import com.ms.parkingcontrol.adapters.dto.PageResponse;
 import com.ms.parkingcontrol.adapters.dto.ParkingSpotRequest;
 import com.ms.parkingcontrol.adapters.dto.ParkingSpotResponse;
+import com.ms.parkingcontrol.domain.parkingmanagement.FilteredParkingSpot;
 import com.ms.parkingcontrol.domain.parkingmanagement.ParkingSpot;
+import com.ms.parkingcontrol.domain.parkingmanagement.ParkingSpotSearch;
+import com.ms.parkingcontrol.domain.parkingmanagement.SortType;
 import com.ms.parkingcontrol.ports.in.parkingmanagement.MongoOperationsPortInbound;
 import com.ms.parkingcontrol.ports.in.parkingmanagement.ParkingSpotFacadePortInbound;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.UUID;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequestMapping("/v1/parking-spots")
 @RestController
@@ -21,6 +28,42 @@ public class ParkingSpotController {
 
     @Autowired
     private MongoOperationsPortInbound mongoOperationsPortInbound;
+
+
+    @GetMapping
+    public ResponseEntity<Object> getAllParkingSpots(@RequestParam(required = false) String parkingSpotNumber, @RequestParam(required = false) String licensePlateCar,
+                                                     @RequestParam(required = false) String responsibleName, @RequestParam(required = false) String brandCar,
+                                                     @RequestParam Integer page, @RequestParam Integer perPage,
+                                                     @RequestParam(value = "sort_type", defaultValue = "asc", required = false) String sortType,
+                                                     @RequestParam(value = "initial_date", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate initialDate,
+                                                     @RequestParam(value = "final_date", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate finalDate) {
+
+        ParkingSpotSearch parkingSpotSearch = ParkingSpotSearch.builder()
+                .withBrandCar(brandCar)
+                .withParkingSpotNumber(parkingSpotNumber)
+                .withPage(page)
+                .withResponsibleName(responsibleName)
+                .withLicensePlateCar(licensePlateCar)
+                .withPerPage(perPage)
+                .withSortType(SortType.fromValue(sortType))
+                .withInitialDate(initialDate)
+                .withFinalDate(finalDate)
+                .build();
+
+
+        FilteredParkingSpot filteredParkingSpot = mongoOperationsPortInbound.getAllParkingSpots(parkingSpotSearch);
+
+        List<ParkingSpotResponse> parkingSpotResponses = filteredParkingSpot.getParkingSpots()
+                .stream()
+                .map(ParkingSpotResponse::from)
+                .collect(Collectors.toList());
+
+        PageResponse<ParkingSpotResponse> parkingSpotResponsePage= new PageResponse<>(
+                parkingSpotResponses, parkingSpotSearch.getPage(), parkingSpotSearch.getPerPage(), filteredParkingSpot.getTotal()
+        );
+
+        return ResponseEntity.ok(parkingSpotResponsePage);
+    }
 
     @PostMapping
     public ResponseEntity<Object> saveParkingSpot(@Valid @RequestBody ParkingSpotRequest parkingSpotRequest) {
