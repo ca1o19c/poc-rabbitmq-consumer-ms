9af97ef01e6649da9439cda5eea793cf3f79ffe1
Caio Antonio <caio.antonio.c@outlook.com>
2022-03-26 22:06:55 -0300

    chore: organized project folders

--- src/main/java/com/ms/parkingcontrol/ports/out/parkingmanagement/ParkingSpotStoreDatabasePortOutbound.java
+++ src/main/java/com/ms/parkingcontrol/ports/out/parkingmanagement/ParkingSpotStoreDatabasePortOutbound.java
@@ -1,15 +1,22 @@
-package com.ms.parkingcontrol.ports.in.parkingmanagement;
+package com.ms.parkingcontrol.ports.out.parkingmanagement;
 
 import com.ms.parkingcontrol.domain.parkingmanagement.FilteredParkingSpot;
 import com.ms.parkingcontrol.domain.parkingmanagement.ParkingSpot;
 import com.ms.parkingcontrol.domain.parkingmanagement.ParkingSpotSearch;
 
-public interface MongoOperationsPortInbound {
+public interface ParkingSpotStoreDatabasePortOutbound {
+
     String findByLicensePlateCar(String licensePlateCar);
+
     String findByParkingSpotNumber(String parkingSpotNumber);
+
     String findByApartment(String apartment);
+
     String findByBlock(String block);
+
     ParkingSpot saveParkingSpot(ParkingSpot parkingSpot);
+
     ParkingSpot getParkingSpot(String id);
+
     FilteredParkingSpot getAllParkingSpots(ParkingSpotSearch parkingSpotSearch);
 }
