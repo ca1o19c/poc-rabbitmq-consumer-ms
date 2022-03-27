71c386b369a38aacb368a6df78359036bb8e1713
Caio Antonio <caio.antonio.c@outlook.com>
2022-03-26 22:06:49 -0300

    chore: organized project folders

--- src/main/java/com/ms/parkingcontrol/adapters/out/parkingmanagement/ResearchedParkingSpot.java
+++ src/main/java/com/ms/parkingcontrol/adapters/out/parkingmanagement/ResearchedParkingSpot.java
@@ -3,17 +3,25 @@ package com.ms.parkingcontrol.adapters.out.parkingmanagement;
 import java.util.List;
 
 public class ResearchedParkingSpot {
+
     private final Integer total;
+
+    private final Integer totalPages;
 
     private final List<ParkingSpot> result;
 
-    public ResearchedParkingSpot(Integer total, List<ParkingSpot> result) {
+    public ResearchedParkingSpot(Integer total, Integer totalPages, List<ParkingSpot> result) {
         this.total = total;
+        this.totalPages = totalPages;
         this.result = result;
     }
 
     public Integer getTotal() {
         return total;
+    }
+
+    public Integer getTotalPages() {
+        return totalPages;
     }
 
     public List<ParkingSpot> getResult() {
